9a7add4dc49c682dfbbac7fdfb15d1a93e514b30
Govind Jangra <115579906+Govind-Jangra@users.noreply.github.com>
2024-07-27 11:38:54 +0530

    Time: 181 ms (55.61%), Space: 98.9 MB (54.63%) - LeetHub

--- 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
+++ 3235-minimum-cost-to-convert-string-i/3235-minimum-cost-to-convert-string-i.cpp
@@ -1,51 +1,24 @@
 class Solution {
 public:
-    long long dp[26][26];
-void solve(int start, vector<pair<int, int>> adj[]) {
-        priority_queue<pair<long long, int>, vector<pair<long long, int>>, greater<pair<long long, int>>> pq;
-        pq.push({0, start});
-        dp[start][start] = 0;
-
-        while (!pq.empty()) {
-            auto [dist, node] = pq.top();
-            pq.pop();
-
-            if (dist > dp[start][node]) continue;
-
-            for (auto [child, cost] : adj[node]) {
-                if (dp[start][child] > dp[start][node] + cost) {
-                    dp[start][child] = dp[start][node] + cost;
-                    pq.push({dp[start][child], child});
-                }
-            }
-        }
-    }
     long long minimumCost(string source, string target, vector<char>& original, vector<char>& changed, vector<int>& cost) {
-        vector<pair<int,int>> adj[26];
-        for(int i=0;i<original.size();i++){
-            adj[original[i]-'a'].push_back({changed[i]-'a',cost[i]});
-        }
-        for(int i=0;i<26;i++){
-            for(int j=0;j<26;j++){
-                dp[i][j]=1e18;
-            }
-        }
-        for(int i=0;i<26;i++){
-            solve(i,adj);
-        }
-
-        long long ans=0;
-        for(int i=0;i<source.size();i++){
-            if(source[i]!=target[i]){
-                if(dp[source[i]-'a'][target[i]-'a']==1e18){
-                    return -1;
-                }
-                ans+=dp[source[i]-'a'][target[i]-'a'];
-            }
-        }
-        return ans;
-         
-
-
+    long long res = 0;
+    vector<vector<long long>> d(26, vector<long long>(26, INT_MAX));
+    for (int i = 0; i < original.size(); ++i) {
+        int s = original[i] - 'a', t = changed[i] - 'a';
+        d[s][t] = min(d[s][t], (long long)cost[i]);
     }
+    for (int k = 0; k < 26; ++k)
+        for (int i = 0; i < 26; ++i)
+            for (int j = 0; j < 26; ++j)
+                d[i][j] = min(d[i][j], d[i][k] + d[k][j]);
+    for (int i = 0; i < source.size(); ++i) {
+        if (source[i] == target[i])
+            continue;
+        int s = source[i] - 'a', t = target[i] - 'a';
+        if (d[s][t] >= INT_MAX)
+            return -1;
+        res += d[s][t];
+    }
+    return res;
+}
 };
